8e3e9400c35d069835a3542aecd2299a72f60994
GlebVaskouski <93393301+GlebVaskouski@users.noreply.github.com>
2022-05-30 00:24:25 +0300

    Add files via upload

--- widget.cpp
+++ widget.cpp
@@ -1,6 +1,5 @@
 #include "widget.h"
 #include "ui_widget.h"
-
 #include "addrecipedialog.h"
 
 #include <QMessageBox>
@@ -13,6 +12,7 @@ Widget::Widget(QWidget *parent)
     ui->setupUi(this);
 
     ui->recipes_listView->setModel(model);
+    ui->recipes_listView->show();
 }
 
 Widget::~Widget()
@@ -23,21 +23,77 @@ Widget::~Widget()
 
 void Widget::on_recipes_listView_clicked(const QModelIndex &index)
 {
-    QMessageBox::warning(this,
-                         tr("\"on_recipes_listView_clicked\" method is not implemented."),
-                         tr("It is one of the methods you need to implement\n"
-                            "to complete the assignment.")
-                         );
+    ui->recipe_name->setText(model->data(index, Qt::DisplayRole).toString());
+    ui->recipe_name->show();
+    std::vector<Recipe> recipes = model->Get();
+    for (auto i = 0; i < recipes[index.row()].ingredients.size(); i++){
+    switch (i){
+    case 0:{
+    ui->in1->setText(recipes[index.row()].ingredients[0].first);
+    ui->spin1->show();
+    ui->spin1->setValue(recipes[index.row()].ingredients[0].second);
+    break;
+    }
+    case 1:{
+        ui->in2->setText(recipes[index.row()].ingredients[1].first);
+        ui->spin2->show();
+        ui->spin2->setValue(recipes[index.row()].ingredients[1].second);
+        break;
+    }
+    case 2:{
+        ui->in3->setText(recipes[index.row()].ingredients[2].first);
+        ui->spin3->show();
+        ui->spin3->setValue(recipes[index.row()].ingredients[2].second);
+        break;
+    }
+    case 3:{
+        ui->in4->setText(recipes[index.row()].ingredients[3].first);
+        ui->spin4->show();
+        ui->spin4->setValue(recipes[index.row()].ingredients[3].second);
+        break;
+    }
+    case 4:{
+        ui->in5->setText(recipes[index.row()].ingredients[4].first);
+        ui->spin1->show();
+        ui->spin5->setValue(recipes[index.row()].ingredients[4].second);
+        break;
+    }
+    }
+    }
+        switch (recipes[index.row()].ingredients.size()){
+        case 0:{
+        ui->in1->clear();
+        ui->spin1->hide();
+        }
+        case 1:{
+            ui->in2->clear();
+            ui->spin2->hide();
+        }
+        case 2:{
+            ui->in3->clear();
+            ui->spin3->hide();
+            }
+        case 3:{
+            ui->in4->clear();
+            ui->spin4->hide();
+            }
+        case 4:{
+            ui->in5->clear();
+            ui->spin5->hide();
+        }
+        default:
+            break;
+        }
+
 }
 
 
 void Widget::on_pushButton_clicked()
 {
     auto * dialog = new AddRecipeDialog();
-    QMessageBox::warning(this,
-                         tr("\"on_pushButton_clicked\" method is partly implemented."),
-                         tr("You need to fully implement the method\n"
-                            "to complete the assignment.")
-                         );
+    dialog->exec();
+    model->addRecipe(dialog->getRecipe());
+    ui->recipes_listView->setModel(model);
+    ui->recipes_listView->show();
 }
 
